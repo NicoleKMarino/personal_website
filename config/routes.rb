6cff103e15bceed891059f58434763240cd56242
Nicole Marino <nicolekmarino@hotmail.com>
2016-10-08 21:38:48 -0600

    project and home views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#show'
+  get '/resume', to: 'resume#show', as: "resume"
+  get '/projects', to: 'projects#index', as: "projects"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.start
+   create_projects
+  end
+
+  def self.create_projects
+    Project.create(name:"City Critic", github_link: "https://github.com/NicoleKMarino/city_critic", production_link:"https://citycritic.herokuapp.com/", image: "citycritic.png", description:"This was a personal project done during my time at Turing. It’s a site which allows a user to view
+potential cities, rate cities, and see ratings by other users. Pulls in third party API data from Yelp, Zillow,
+and Facebook. Uses Javascript and Bower code to implement the rating system. I also implemented
+caching in the background to improve the performance of page loading.")
+    Project.create(name:"Make My Payday", github_link: "https://github.com/NicoleKMarino/the_pivot", production_link:"https://makemypayday.herokuapp.com/", image: "makemypayday.png", description: "This was a application produced by a team of Turing students. It’s a website dedicated to job hunting
+based on a specific location, company, or salary range. Allows different levels of access to the site based
+on multi-tenancy authentication. Able to sign in via twitter accounts, and sends client emails verifications.
+Functionality developed using Javascript, Ruby on Rails, PostgreSQL, Bcrypt, and Bootstrap.")
+    Project.create(name:"Westeros Outfitters", github_link: "https://github.com/NicoleKMarino/westeros_outfitters", production_link:"https://westerosoutfitters.herokuapp.com/", image: "westeros.png", description: "A mock E-Commerce site. Allows users to purchase itesma and check out with swipe. Sends confirmation emails for purchasing and signing up. Functionality built with Ruby on Rails, practices CRUD functionality")
+    Project.create(name:"Sales Engine", github_link: "https://github.com/NicoleKMarino/rales_engine", image: "ralesengine.png", description:"Sales Engine was a project done by me and one other developer. RESTful API to serve up sales data in
+JSON format, using Ruby on Rails, ActiveRecord, SQL queries, and Jbuilder. Instructions on how to
+retrieve the data and to see the spec it was based off of can be found on the github.")
+  end
+end
+Seed.start
